563dffab3c1bef99238336ac09a0b9177719ffa0
khantim-git <154319896+khantim-git@users.noreply.github.com>
2024-10-11 15:48:18 +0700

    Update SqlFile.sql

--- SplitCSV/SqlFile.sql
+++ SplitCSV/SqlFile.sql
@@ -1,2 +1,3 @@
-select *
-from abaaa
+-- Create text widgets for schema and table names
+CREATE WIDGET TEXT schema_name DEFAULT 'default_schema' COMMENT 'Enter Schema Name';
+CREATE WIDGET TEXT table_name DEFAULT 'AAA_BBB' COMMENT 'Enter Table Name';
