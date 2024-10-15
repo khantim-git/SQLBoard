f44ca69116c8bbe9083533ad500d4c55c6f68958
khantim-git <154319896+khantim-git@users.noreply.github.com>
2024-10-15 11:37:31 +0700

    Update SqlFile.sql

--- SplitCSV/SqlFile.sql
+++ SplitCSV/SqlFile.sql
@@ -1,3 +1,17 @@
 -- Create text widgets for schema and table names
 CREATE WIDGET TEXT schema_name DEFAULT 'default_schema' COMMENT 'Enter Schema Name';
 CREATE WIDGET TEXT table_name DEFAULT 'AAA_BBB' COMMENT 'Enter Table Name';
+
+
+table_name = "database_name.table_name"  # Thay bằng tên bảng của bạn
+
+# Lấy thông tin phân vùng của bảng
+df = spark.sql(f"DESCRIBE FORMATTED {table_name}")
+
+# Kiểm tra xem bảng có trường 'Partition' không
+partitions = df.filter(df.col("col_name") == "Partition Columns")
+
+if partitions.count() > 0:
+    print(f"{table_name} là bảng phân vùng.")
+else:
+    print(f"{table_name} không phải là bảng phân vùng.")
