7b14255a25f937406fa1e1c7e539fc3cd2f5c774
khantim-git <154319896+khantim-git@users.noreply.github.com>
2024-10-18 11:20:28 +0700

    Update SqlFile.sql

--- SplitCSV/SqlFile.sql
+++ SplitCSV/SqlFile.sql
@@ -1,17 +1,4 @@
--- Create text widgets for schema and table names
-CREATE WIDGET TEXT schema_name DEFAULT 'default_schema' COMMENT 'Enter Schema Name';
-CREATE WIDGET TEXT table_name DEFAULT 'AAA_BBB' COMMENT 'Enter Table Name';
-
-
-table_name = "database_name.table_name"  # Thay bằng tên bảng của bạn
-
-# Lấy thông tin phân vùng của bảng
-df = spark.sql(f"DESCRIBE FORMATTED {table_name}")
-
-# Kiểm tra xem bảng có trường 'Partition' không
-partitions = df.filter(df.col("col_name") == "Partition Columns")
-
-if partitions.count() > 0:
-    print(f"{table_name} là bảng phân vùng.")
-else:
-    print(f"{table_name} không phải là bảng phân vùng.")
+SELECT MIN(work_date) AS first_working_day
+FROM working_day
+WHERE work_date >= DATE_FORMAT(CURRENT_DATE(), 'yyyy-MM-01')
+AND work_date < DATE_ADD(LAST_DAY(CURRENT_DATE()), 1)
